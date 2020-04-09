bebd1168381365bf3e52624d7abc414fccc3b90f
HanHyunJin-mis <forinpea121@gmail.com>
2020-04-09 22:11:27 +0900

    Feat : gyrePrint function think again

--- 5._gyre-print.js
+++ 5._gyre-print.js
@@ -26,14 +26,21 @@ function jaggedArray(row, column) {
 console.log(jaggedArray(4, 5));
 
 // 나선형으로 출력하기
-let row = 4;
-let column = 5;
 
-function gyrePrint(row, column) {
-  let tempRow = row; // 4
-  let tempColumn = column; // 5
-  for (let i = 0; i < row * column; i++) {}
-  console.log(newArray[tempRow][tempColumn]);
+// 가로 이동 -> 세로 이동 -> 가로 이동 -> 세로 이동 ...
+// 가로와 세로 이동을 한번씩 줄여가기
+// 가로 변수와 세로 변수를 만들어 반복문이 돌 때마다 변수 -1을 하면 된다고 생각했다.
+
+// 책에 나와있는 방법
+// - 왼쪽에서 오른쪽으로 출력하기
+// - 위에서 아래쪽으로 출력하기
+// - 오른쪽에서 왼쪽으로 출력하기
+// - 아래쪽에서 위쪽으로 출력하기
+function gyrePrint(array) {
+  let topRow = 0;
+  let leftCol = 0;
+  let btmRow = array.length - 1;
+  let rightCol = array[0].length - 1;
 }
 
-gyrePrint(row, column);
+gyrePrint(newArray);
