a3d1de44f04e864c97ba01fa1621f0f93fdc649b
HanHyunJin-mis <forinpea121@gmail.com>
2020-04-09 21:45:30 +0900

    Feat : make gyre array

--- 5._gyre-print.js
+++ 5._gyre-print.js
@@ -1,1 +1,24 @@
-//
+// 나선형으로 출력
+
+// 증가하는 2중 배열 만들기
+
+function jaggedArray(row, column) {
+  let newArray = new Array(row);
+
+  for (let i = 0; i < row; i++) {
+    newArray[i] = new Array(column);
+  }
+
+  let rowCount = 0;
+  let columnCount = 0;
+  for (let j = 1; j <= row * column; j++) {
+    if (column === columnCount) {
+      rowCount += 1;
+      columnCount = 0;
+    }
+    newArray[rowCount][columnCount] = j;
+    columnCount++;
+  }
+}
+
+jaggedArray(3, 4);
