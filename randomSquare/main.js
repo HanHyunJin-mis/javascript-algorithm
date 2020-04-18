b54bf9609eeab7d25e7ea3bc5f08d0dc491843ff
HanHyunJin-mis <forinpea121@gmail.com>
2020-04-18 10:29:53 +0900

    Feat : add changeColors function

--- randomSquare/main.js
+++ randomSquare/main.js
@@ -1,1 +1,42 @@
 console.log("안녕 좋은아침ㅎㅎ");
+
+let $squareFrame = document.querySelector(".squares-frame");
+
+window.onload = function () {
+  console.log("온로드!", $squareFrame);
+  let squareCount = parseInt(Math.random() * 50 + 1);
+
+  console.log("0~50 랜덤 수", squareCount);
+  for (let i = 0; i < squareCount; i++) {
+    addSquare();
+  }
+};
+
+function addSquare() {
+  let square = document.createElement("div");
+  square.className = "square";
+  square.style.left = parseInt(Math.random() * 1500) + "px";
+  square.style.top = parseInt(Math.random() * 700) + "px";
+
+  square.style.backgroundColor = getRandomColor();
+  let squareArea = document.getElementById("squares-frame");
+  console.log(squareArea, square);
+
+  squareArea.appendChild(square);
+}
+
+function getRandomColor() {
+  let letters = "0123456789abcdef";
+  let result = "#";
+  for (let i = 0; i < 6; i++) {
+    result += letters.charAt(parseInt(Math.random() * letters.length));
+  }
+  console.log(result);
+
+  return result;
+}
+
+function changeColors() {
+  let squares = document.querySelectorAll("#squares-frame div");
+  for (let i = 0; i < squares.length; i++) {}
+}
