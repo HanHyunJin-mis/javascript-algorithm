525976ff925bf91b323be7a5964c4ae3dc04abb1
HanHyunJin-mis <forinpea121@gmail.com>
2020-09-06 23:57:23 +0900

    Feat : add quicksort.js

---
+++ 200906/quick sort.js
@@ -0,0 +1,9 @@
+let arr = [10, 1, 5, 8, 7, 6, 4, 3, 2, 9];
+function quickSort(arr, start, end) {
+  // 필요한 변수 : start, end, mid
+  // 왼쪽 -> 오른쪽으로 옮길 변수 left와 오른쪽 -> 왼쪽으로 옮길 변수 right
+  let left = start;
+  let right = end;
+  // 재귀
+}
+console.log(quickSort(arr, 0, arr.length - 1));
