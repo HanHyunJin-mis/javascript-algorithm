cf816e42e7ffa86b26d6063a55127ff1d5570f1e
HanHyunJin-mis <forinpea121@gmail.com>
2020-09-07 00:06:38 +0900

    Feat : find the value to be exchanged

--- 200906/quick sort.js
+++ 200906/quick sort.js
@@ -4,6 +4,19 @@ function quickSort(arr, start, end) {
   // 왼쪽 -> 오른쪽으로 옮길 변수 left와 오른쪽 -> 왼쪽으로 옮길 변수 right
   let left = start;
   let right = end;
+  // 중간값 정하기
+  let mid = parseInt((left + right) / 2);
+  // left값이 right 값을 넘지 않아야 함
+  while (left <= right) {
+    // left 값이 중간값을 넘는 경우 찾기
+    while (arr[left] < arr[mid]) {
+      left++;
+    }
+    // right 값이 중간값을 넘는 경우 찾기
+    while (arr[right] > arr[mid]) {
+      right--;
+    }
+  }
 
   // 재귀
   quickSort(arr, start, right);
