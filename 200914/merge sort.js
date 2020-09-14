80acc9f234bbb63e288e942d680a397b5fde4faa
HanHyunJin-mis <forinpea121@gmail.com>
2020-09-15 00:47:44 +0900

    Feat : merge sort basic

--- 200914/merge sort.js
+++ 200914/merge sort.js
@@ -1,1 +1,13 @@
-function mergeSort() {}
+function merge(arr, start, mid, end) {}
+function mergeSort(arr, start, end) {
+  // 쪼개지는 과정 재귀,
+  if (start > end) {
+    let mid = parseInt((start + end) / 2); //중간값구하기
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+    // 다 쪼개었을 때 merge
+    merge(arr, start, mid, end);
+  }
+}
+let arr = [10, 1, 5, 8, 7, 6, 4, 3, 2, 9];
+console.log(mergeSort(arr, 0, arr.length - 1));
